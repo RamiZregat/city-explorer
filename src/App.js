214dd031ec61385acf667c5eb291b0c1344113b8
RamiZregat <rami.zregat97@gmail.com>
2021-09-06 23:52:59 +0300

    doing the lab

--- src/App.js
+++ src/App.js
@@ -13,16 +13,50 @@ class App extends React.Component {
       lat:'',
       display_name:'',
       disPlayError: false,
-      mapDisplay:false
+      mapDisplay:false,
+      timezone:'',
+      countrycode:'',
+      statecode:'',
+      lonWeather:'',
+      latWeather:'',
+      Weather:false,
+      disPlayWeatherError:false
+    }
+  }
+  getWeatherData=async(CityName)=>{
+    let WeatherURL=`http://localhost:3010/weather?cityname=${CityName}`;
+
+    try{
+      if(CityName==='Amman'||CityName==='Paris'||CityName==='Seattle'){
+      let WeatherData=await axios.get(WeatherURL)
+      this.setState({
+        timezone:WeatherData.data.timezone,
+        countrycode:WeatherData.data.country_code,
+        statecode:WeatherData.data.state_code,
+        lonWeather:WeatherData.data.lon,
+        latWeather:WeatherData.data.lat,
+        Weather:true
+      })
+    }else{
+      this.setState({
+        disPlayWeatherError:true
+      })
+    }}
+    catch{
+      this.setState({
+        disPlayWeatherError:true
+      })
     }
   }
 
   getCityData=async(event)=>{
     event.preventDefault();
     let CityName=event.target.cityName.value;
-    let Key="pk.187e19748e6b7d412b6f784bf36db13e"
-    let URL=`https://eu1.locationiq.com/v1/search.php?key=${Key}&q=${CityName}&format=json`
+    this.getWeatherData(CityName);
+    let Key="pk.e7bf618f9e0781f31c85230b2a6a24f0";
+    let URL=`https://eu1.locationiq.com/v1/search.php?key=${Key}&q=${CityName}&format=json`;
     
+  
 
     try{
     let Data= await axios.get(URL);
@@ -40,6 +74,8 @@ class App extends React.Component {
       })
     }
   }
+
+
 
   
 
@@ -122,8 +158,17 @@ class App extends React.Component {
         <p style={Pstyle}>Latitude:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.lat}</p>
         <p style={Pstyle}>Longitude:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.lon}</p>
         </div>
+        {this.state.mapDisplay && <Image style={MapStyling} src={`https://maps.locationiq.com/v3/staticmap?key=pk.e7bf618f9e0781f31c85230b2a6a24f0&center=${this.state.lat},${this.state.lon}&zoom=${1-18}`} alt='map' fluid />}
         {this.state.disPlayError && <p style={errorStyle}>Sorry Error</p>}
-        {this.state.mapDisplay && <Image style={MapStyling} src={`https://maps.locationiq.com/v3/staticmap?key=pk.187e19748e6b7d412b6f784bf36db13e&center=${this.state.lat},${this.state.lon}&zoom=${1-18}`} alt='map' fluid />}
+        {this.state.Weather&&<p style={Pstyle}>City name:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.display_name}</p>}
+        {this.state.Weather&&<p style={Pstyle}>Latitude:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.latWeather}</p>}
+        {this.state.Weather&&<p style={Pstyle}>Longitude:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.lonWeather}</p>}
+        {this.state.Weather&&<p style={Pstyle}>Time Zone:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.timezone}</p>}
+        {this.state.Weather&&<p style={Pstyle}>Country Code:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.countrycode}</p>}
+        {this.state.Weather&&<p style={Pstyle}>State Code:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.statecode}</p>}
+        {this.state.disPlayWeatherError&&<p style={errorStyle}>Sorry Error</p>}
+
+        
       </>
     );
   }
