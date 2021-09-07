b04156b18787513535f086e537cfeb0116709a33
RamiZregat <rami.zregat97@gmail.com>
2021-09-07 16:41:25 +0300

    finished lab07

--- src/App.js
+++ src/App.js
@@ -14,34 +14,23 @@ class App extends React.Component {
       display_name:'',
       disPlayError: false,
       mapDisplay:false,
-      timezone:'',
-      countrycode:'',
-      statecode:'',
-      lonWeather:'',
-      latWeather:'',
       Weather:false,
-      disPlayWeatherError:false
+      disPlayWeatherError:false,
+      weatherArray:[]
     }
   }
-  getWeatherData=async(CityName)=>{
-    let WeatherURL=`https://rami-city-explorer.herokuapp.com/weather?cityname=${CityName}`;
+  getWeatherData=async(lat,lon)=>{
+    let WeatherURL=`https://rami-city-explorer.herokuapp.com/weather?lat=${lat}&lon=${lon}`;
 
     try{
-      if(CityName==='Amman'||CityName==='Paris'||CityName==='Seattle'){
+      
       let WeatherData=await axios.get(WeatherURL)
+      
       this.setState({
-        timezone:WeatherData.data.timezone,
-        countrycode:WeatherData.data.country_code,
-        statecode:WeatherData.data.state_code,
-        lonWeather:WeatherData.data.lon,
-        latWeather:WeatherData.data.lat,
+        weatherArray:WeatherData.data,
         Weather:true
       })
-    }else{
-      this.setState({
-        disPlayWeatherError:true
-      })
-    }}
+    }
     catch{
       this.setState({
         disPlayWeatherError:true
@@ -49,17 +38,17 @@ class App extends React.Component {
     }
   }
 
-  getCityData=async(event)=>{
+  getCityData=async(event)=>{ 
     event.preventDefault();
     let CityName=event.target.cityName.value;
-    this.getWeatherData(CityName);
-    let Key="pk.e7bf618f9e0781f31c85230b2a6a24f0";
-    let URL=`https://eu1.locationiq.com/v1/search.php?key=${Key}&q=${CityName}&format=json`;
+    let Key=process.env.REACT_APP_Key;
+    let URL=`https://eu1.locationiq.com/v1/search.php?key=pk.43fed3791d35ddb76aa14f749c6d3080&q=${CityName}&format=json`;
     
   
 
     try{
     let Data= await axios.get(URL);
+    
     this.setState({
       lon:Data.data[0].lon,
       lat:Data.data[0].lat,
@@ -67,18 +56,18 @@ class App extends React.Component {
       mapDisplay:true
     
     })
+    this.getWeatherData(this.state.lat,this.state.lon)
+    
+  
     }
+    
     catch{
       this.setState({
         disPlayError: true
       })
     }
+    
   }
-
-
-
-  
-
   render() {
     
 
@@ -158,14 +147,18 @@ class App extends React.Component {
         <p style={Pstyle}>Latitude:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.lat}</p>
         <p style={Pstyle}>Longitude:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.lon}</p>
         </div>
-        {this.state.mapDisplay && <Image style={MapStyling} src={`https://maps.locationiq.com/v3/staticmap?key=pk.e7bf618f9e0781f31c85230b2a6a24f0&center=${this.state.lat},${this.state.lon}&zoom=${1-18}`} alt='map' fluid />}
+        {this.state.mapDisplay && <Image style={MapStyling} src={`https://maps.locationiq.com/v3/staticmap?key=pk.43fed3791d35ddb76aa14f749c6d3080&center=${this.state.lat},${this.state.lon}&zoom=${1-18}`} alt='map' fluid />}
         {this.state.disPlayError && <p style={errorStyle}>Sorry Error</p>}
-        {this.state.Weather&&<p style={Pstyle}>City name:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.display_name}</p>}
-        {this.state.Weather&&<p style={Pstyle}>Latitude:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.latWeather}</p>}
-        {this.state.Weather&&<p style={Pstyle}>Longitude:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.lonWeather}</p>}
-        {this.state.Weather&&<p style={Pstyle}>Time Zone:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.timezone}</p>}
-        {this.state.Weather&&<p style={Pstyle}>Country Code:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.countrycode}</p>}
-        {this.state.Weather&&<p style={Pstyle}>State Code:&nbsp;&nbsp;&nbsp;&nbsp;{this.state.statecode}</p>}
+
+        {this.state.Weather && this.state.weatherArray.map(item =>{
+          return(
+            <>
+          <p style={Pstyle}>Date:&nbsp;&nbsp;{item.date}</p>
+          <p style={Pstyle}>Description:&nbsp;&nbsp;{item.description}</p>
+          </>
+          )
+        })}
+
         {this.state.disPlayWeatherError&&<p style={errorStyle}>Sorry Error</p>}
 
         
